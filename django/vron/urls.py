d88a08f2bcb446ffed546a1c6c14c65f8582cba7
avictorino <ademar@nakedbookings.com>
2016-06-28 00:55:50 +1000

    Adjust URLs to local and server

--- django/vron/urls.py
+++ django/vron/urls.py
@@ -1,6 +1,5 @@
 """
 Base URLs for all apps
-
 https://docs.djangoproject.com/en/1.7/topics/http/urls/
 """
 
@@ -11,11 +10,9 @@ from django.conf.urls import url, patterns, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.http import HttpResponseRedirect
-import os
 
 
 
-#print os.path.abspath(os.path.join(os.path.dirname(__file__), "../", "static"))
 
 
 ##########################
@@ -26,6 +23,4 @@ urlpatterns = patterns('',
     url( r'^admin/', include( 'vron.admin.urls', namespace = "admin" ) ),
     url( r'^$', lambda r: HttpResponseRedirect( 'admin/' ) ),
     url( r'^connector/', include( 'vron.connector.urls', namespace = "connector" ) ),
-    #url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': os.path.join(os.path.dirname(__file__),
-                                                                                              # "../", "static")}),
 ) + static( settings.MEDIA_URL, document_root = settings.MEDIA_ROOT )
