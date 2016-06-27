3280e967c223651eee97ef02ea6e541838bcf920
avictorino <ademar@nakedbookings.com>
2016-06-28 00:59:34 +1000

    Adjust settings file

--- django/vron/_settings/custom.py
+++ django/vron/_settings/custom.py
@@ -71,12 +71,12 @@ INTERNAL_IPS = (
 #########################################
 
 # SMTP Config
-EMAIL_PAYMENT_OPTION_TO = ["support@respax.com"]
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = '#'
-EMAIL_HOST_PASSWORD = '#'
-EMAIL_PORT = 587
+#EMAIL_PAYMENT_OPTION_TO = ["support@respax.com"]
+#EMAIL_USE_TLS = True
+#EMAIL_HOST = 'smtp.gmail.com'
+#EMAIL_HOST_USER = '#'
+#EMAIL_HOST_PASSWORD = '#'
+#EMAIL_PORT = 587
 
 
 #########################################
